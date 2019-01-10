30053cbf4e5b01512bccacf6f16c72fe32bfdbe9
UsAgile04(Okuro) <yman.0393@gmail.com>
2019-01-11 02:59:20 +0900

    やったぜ。SNSメニューの件はなんとかなった。あとはSNS連携の機能面を拡充させていく。

--- js/dropdown_menu.js
+++ js/dropdown_menu.js
@@ -1,12 +1,12 @@
 $(function () {
 
     //global nav
-    var btn = $(".parent");
-    var submenu = $(".mod_dropnavi_child");
-    var submenulink = $(".mod_dropnavi_child ul li a");
+    var btn = $(".exhibition-show__info-foot-menu-btn");
+    var submenu = $(".exhibition-show__info-foot-menu");
+    var submenulink = $(".exhibition-show__info-foot-menu li a");
     //click
     $(btn).bind("click", "focus", function (event) {
-        var shownav = $(this).find(".mod_dropnavi_child");
+        var shownav = $(this).find(".exhibition-show__info-foot-menu");
         if ($(shownav).css("display") == "none") {
             $(shownav).slideDown("fast");
         } else {
